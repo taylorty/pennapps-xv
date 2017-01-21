ef52b9ef668caa3269f85e4f684da9b141c7a6df
ysx001 <ysx001@bucknell.edu>
2017-01-21 08:10:05 -0500

    Edited Main Activity to work with Google fit

---
+++ CatMyLife/app/src/main/java/com/catmylife/android/catmylife/MainActivity_2.java
@@ -0,0 +1,46 @@
+package com.catmylife.android.catmylife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+public class MainActivity extends AppCompatActivity implements OnDataPointListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    private static final int REQUEST_OAUTH = 1;
+    private static final String AUTH_PENDING = "auth_state_pending";
+    private boolean authInProgress = false;
+    private GoogleApiClient mApiClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onDataPoint(DataPoint dataPoint) {
+
+    }
+}
