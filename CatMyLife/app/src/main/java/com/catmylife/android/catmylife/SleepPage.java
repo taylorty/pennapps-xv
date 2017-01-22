c79cd05cac469cf07d648672432ee17949913460
khaiquangnguyen <kqn001@bucknell.edu>
2017-01-22 07:10:58 -0500

    Merge branch 'animation'
    # Conflicts:
    #	CatMyLife/.gradle/2.14.1/taskArtifacts/cache.properties.lock
    #	CatMyLife/.gradle/2.14.1/taskArtifacts/fileHashes.bin
    #	CatMyLife/.gradle/2.14.1/taskArtifacts/fileSnapshots.bin
    #	CatMyLife/.gradle/2.14.1/taskArtifacts/fileSnapshotsToTreeSnapshotsIndex.bin
    #	CatMyLife/.gradle/2.14.1/taskArtifacts/taskArtifacts.bin
    #	CatMyLife/.idea/workspace.xml

--- CatMyLife/app/src/main/java/com/catmylife/android/catmylife/SleepPage.java
+++ CatMyLife/app/src/main/java/com/catmylife/android/catmylife/SleepPage.java
@@ -1,21 +1,38 @@
 package com.catmylife.android.catmylife;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 public class SleepPage extends AppCompatActivity {
     ProgressBar progressBar;
     Button start_timer, stop_timer;
     MyCountDownTimer myCountDownTimer;
+    ImageView normalCat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
+        normalCat = (ImageView) findViewById(R.id.catImageView);
+        if (Cat.weight == 1) {
+            switchToSuperThinCat();
+        } else if (Cat.weight == 2) {
+            switchToThinCat();
+        } else if (Cat.weight == 3) {
+            switchToNormalCat();
+        } else if (Cat.weight == 4) {
+            switchToFatCat();
+        }
+        AnimationDrawable normalCatAnimation = (AnimationDrawable) normalCat.getDrawable();
+        normalCatAnimation.setCallback(normalCat);
+        normalCatAnimation.setVisible(true, true);
+        normalCatAnimation.start();
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         start_timer = (Button) findViewById(R.id.button);
@@ -34,7 +51,6 @@ public class SleepPage extends AppCompatActivity {
         stop_timer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 myCountDownTimer.cancel();
 
             }
@@ -57,9 +73,43 @@ public class SleepPage extends AppCompatActivity {
         }
 
         @Override
-        public void onFinish() {
+            public void onFinish() {
             finish();
         }
     }
 
+    private void switchToFatCat() {
+        normalCat.setImageResource(R.drawable.sleep_fat_animation);
+        AnimationDrawable normalCatAnimation = (AnimationDrawable) normalCat.getDrawable();
+        normalCatAnimation.setCallback(normalCat);
+        normalCatAnimation.setVisible(true, true);
+        normalCatAnimation.start();
+    }
+
+    private void switchToNormalCat() {
+        normalCat.setImageResource(R.drawable.sleep_normal_animation);
+        AnimationDrawable normalCatAnimation = (AnimationDrawable) normalCat.getDrawable();
+        normalCatAnimation.setCallback(normalCat);
+        normalCatAnimation.setVisible(true, true);
+        normalCatAnimation.start();
+    }
+
+    private void switchToThinCat() {
+        normalCat.setImageResource(R.drawable.sleep_thin_animation);
+        AnimationDrawable normalCatAnimation = (AnimationDrawable) normalCat.getDrawable();
+        normalCatAnimation.setCallback(normalCat);
+        normalCatAnimation.setVisible(true, true);
+        normalCatAnimation.start();
+    }
+
+    private void switchToSuperThinCat() {
+        normalCat.setImageResource(R.drawable.sleep_super_thin_animation);
+        AnimationDrawable normalCatAnimation = (AnimationDrawable) normalCat.getDrawable();
+        normalCatAnimation.setCallback(normalCat);
+        normalCatAnimation.setVisible(true, true);
+        normalCatAnimation.start();
+
+    }
 }
+
+
